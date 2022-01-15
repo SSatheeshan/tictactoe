368fb61989d1e99cd7df5f30ee7390d7da9e0c09
Satheeshan <satheeshan.satheesh26@gmail.com>
2022-01-15 14:28:49 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const app = () => {
+  return (
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>Hard to get more minimal than this React app.</p>
+      <small>It's so tiny to get</small>
+    </>
+  );
+};
+export default app;
